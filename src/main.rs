de2a1da6f4f01fb250c70132138f820f0e226cfb
Michael Cartwright <cartwmic@gmail.com>
2023-12-10 23:10:02 -0800

    initial stubbing of routine add/edit/delete endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use askama::Template;
 use axum::{
+    extract::Path,
     http::StatusCode,
     response::{Html, IntoResponse},
-    routing::get,
+    routing::{delete, get, patch, post, put},
     Router,
 };
 use serde::{Deserialize, Serialize};
-use std::time::{Duration, Instant};
+use uuid::Uuid;
 
 #[tokio::main]
 async fn main() {
@@ -16,7 +17,10 @@ async fn main() {
     // build our application with a route
     let app = Router::new()
         // `GET /` goes to `root`
-        .route("/", get(root));
+        .route("/", get(root))
+        .route("/routine/:routine_id", put(edit_routine))
+        .route("/routine/:routine_id", delete(delete_routine))
+        .route("/routine", post(add_routine));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -25,16 +29,56 @@ async fn main() {
 
 // basic handler that responds with a static string
 async fn root() -> impl IntoResponse {
-    let template = IndexTemplate {};
+    let template = IndexTemplate {
+        routines: vec![
+            Routine { id: Uuid::new_v4() },
+            Routine { id: Uuid::new_v4() },
+        ],
+    };
+    let reply_html = template.render().unwrap();
+    (StatusCode::OK, Html(reply_html).into_response())
+}
+
+async fn edit_routine(Path(routine_id): Path<Uuid>) -> impl IntoResponse {
+    let template = IndexTemplate {
+        routines: vec![
+            Routine { id: Uuid::new_v4() },
+            Routine { id: Uuid::new_v4() },
+        ],
+    };
+    let reply_html = template.render().unwrap();
+    (StatusCode::OK, Html(reply_html).into_response())
+}
+
+async fn delete_routine(Path(routine_id): Path<Uuid>) -> impl IntoResponse {
+    let template = IndexTemplate {
+        routines: vec![
+            Routine { id: Uuid::new_v4() },
+            Routine { id: Uuid::new_v4() },
+        ],
+    };
+    let reply_html = template.render().unwrap();
+    (StatusCode::OK, Html(reply_html).into_response())
+}
+
+async fn add_routine() -> impl IntoResponse {
+    let template = IndexTemplate {
+        routines: vec![
+            Routine { id: Uuid::new_v4() },
+            Routine { id: Uuid::new_v4() },
+        ],
+    };
     let reply_html = template.render().unwrap();
     (StatusCode::OK, Html(reply_html).into_response())
 }
 
 #[derive(Template)]
 #[template(path = "index.html")]
-struct IndexTemplate {}
+struct IndexTemplate {
+    routines: Vec<Routine>,
+}
 
 #[derive(Serialize, Deserialize)]
 struct Routine {
-    id: String,
+    id: Uuid,
 }
